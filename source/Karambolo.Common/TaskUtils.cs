d84a07c8e0cc46486c6c47e4cf096b29a6df4597
adams85 <adamosimoni@gmail.com>
2018-10-25 20:20:54 +0200

    TaskUtils revised + full test coverage - [BC] FromCancelled renamed to FromCanceled, AsCancellable renamed to AsCancelable to match the framework naming convention - CompletedTask backport - WithTimeout: msec convenient overloads added

--- source/Karambolo.Common/TaskUtils.cs
+++ source/Karambolo.Common/TaskUtils.cs
@@ -5,18 +5,22 @@ using System.Threading.Tasks;
 
 namespace Karambolo.Common
 {
-    public static partial class TaskUtils
+    public static class TaskUtils
     {
-#if NET40
+#if NET45 || NETSTANDARD1_0
+        public static Task CompletedTask { get; } = Task.FromResult<object>(null);
+#elif NET40
+        public static Task CompletedTask { get; } = FromResult<object>(null);
+
         public static Task<TResult> FromResult<TResult>(TResult result)
         {
             var tcs = new TaskCompletionSource<TResult>();
-            tcs.TrySetResult(result);
+            tcs.SetResult(result);
             return tcs.Task;
         }
 #endif
 
-#if NET40 || NET45
+#if NET40 || NET45 || NETSTANDARD1_0
         public static Task FromException(Exception exception)
         {
             return FromException<object>(exception);
@@ -28,23 +32,189 @@ namespace Karambolo.Common
                 throw new ArgumentNullException(nameof(exception));
 
             var tcs = new TaskCompletionSource<TResult>();
-            tcs.TrySetException(exception);
+            tcs.SetException(exception);
             return tcs.Task;
         }
 
-        public static Task FromCancelled(CancellationToken cancellationToken)
+        public static Task FromCanceled(CancellationToken cancellationToken)
         {
-            return FromCancelled<object>(cancellationToken);
+            return FromCanceled<object>(cancellationToken);
         }
 
-        public static Task<TResult> FromCancelled<TResult>(CancellationToken cancellationToken)
+        public static Task<TResult> FromCanceled<TResult>(CancellationToken cancellationToken)
         {
             if (!cancellationToken.IsCancellationRequested)
                 throw new ArgumentOutOfRangeException(nameof(cancellationToken));
 
             var tcs = new TaskCompletionSource<TResult>();
-            tcs.TrySetCanceled();
+            tcs.SetCanceled();
             return tcs.Task;
+        }
+#else
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+        static Task<TResult> FromCanceled<TResult>(CancellationToken cancellationToken)
+        {
+            return Task.FromCanceled<TResult>(cancellationToken);
+        }
+#endif
+
+#if !NETSTANDARD1_0
+        public static Task WaitForExitAsync(this Process process)
+        {
+            var tcs = new TaskCompletionSource<object>();
+
+            EventHandler exitedHandler = null;
+            exitedHandler = (s, e) =>
+            {
+                process.Exited -= exitedHandler;
+                tcs.TrySetResult(null);
+            };
+
+            process.EnableRaisingEvents = true;
+            process.Exited += exitedHandler;
+
+            return tcs.Task;
+        }
+#endif
+
+#if !NET40
+        #region Timeout
+
+        internal static async Task WithTimeoutAsync(Task task, TimeSpan timeout)
+        {
+            var timeoutTask = Task.Delay(timeout);
+            var completedTask = await Task.WhenAny(task, timeoutTask).ConfigureAwait(false);
+
+            if (completedTask == timeoutTask)
+                throw new TimeoutException();
+
+            await completedTask.ConfigureAwait(false);
+        }
+
+        public static Task WithTimeout(this Task task, TimeSpan timeout)
+        {
+            if (task == null)
+                throw new NullReferenceException();
+
+            return WithTimeoutAsync(task, timeout);
+        }
+
+        public static Task WithTimeout(this Task task, int millisecondsTimeout)
+        {
+            return task.WithTimeout(TimeSpan.FromTicks(millisecondsTimeout * TimeSpan.TicksPerMillisecond));
+        }
+
+        internal static async Task<TResult> WithTimeoutAsync<TResult>(Task<TResult> task, TimeSpan timeout)
+        {
+            var timeoutTask = Task.Delay(timeout);
+            var completedTask = await Task.WhenAny(task, timeoutTask).ConfigureAwait(false);
+
+            if (completedTask == timeoutTask)
+                throw new TimeoutException();
+
+            return await ((Task<TResult>)completedTask).ConfigureAwait(false);
+        }
+
+        public static Task<TResult> WithTimeout<TResult>(this Task<TResult> task, TimeSpan timeout)
+        {
+            if (task == null)
+                throw new NullReferenceException();
+
+            return WithTimeoutAsync(task, timeout);
+        }
+
+        public static Task<TResult> WithTimeout<TResult>(this Task<TResult> task, int millisecondsTimeout)
+        {
+            return task.WithTimeout(TimeSpan.FromTicks(millisecondsTimeout * TimeSpan.TicksPerMillisecond));
+
+        }
+
+        #endregion
+
+        #region Cancellation
+
+        readonly struct CancellationTokenTaskSource<TResult> : IDisposable
+        {
+            readonly IDisposable _ctr;
+
+            public CancellationTokenTaskSource(CancellationToken cancellationToken)
+            {
+                if (!cancellationToken.IsCancellationRequested)
+                {
+                    var tcs = new TaskCompletionSource<TResult>();
+                    _ctr = cancellationToken.Register(() => tcs.TrySetCanceled(), useSynchronizationContext: false);
+                    Task = tcs.Task;
+                }
+                else
+                {
+                    _ctr = null;
+                    Task = FromCanceled<TResult>(cancellationToken);
+                }
+            }
+
+            public Task<TResult> Task { get; }
+
+            public void Dispose()
+            {
+                _ctr?.Dispose();
+            }
+        }
+
+        internal static async Task AsCancelableAsync(Task task, CancellationToken cancellationToken)
+        {
+            using (var ctts = new CancellationTokenTaskSource<object>(cancellationToken))
+                await await Task.WhenAny(ctts.Task, task).ConfigureAwait(false);
+        }
+
+        public static Task AsCancelable(this Task task, CancellationToken cancellationToken)
+        {
+            if (task == null)
+                throw new NullReferenceException();
+
+            if (!cancellationToken.CanBeCanceled)
+                return task;
+
+            if (cancellationToken.IsCancellationRequested)
+                return FromCanceled<object>(cancellationToken);
+
+            return AsCancelableAsync(task, cancellationToken);
+        }
+
+        internal static async Task<TResult> AsCancelableAsync<TResult>(Task<TResult> task, CancellationToken cancellationToken)
+        {
+            using (var ctts = new CancellationTokenTaskSource<TResult>(cancellationToken))
+                return await await Task.WhenAny(ctts.Task, task).ConfigureAwait(false);
+        }
+
+        public static Task<TResult> AsCancelable<TResult>(this Task<TResult> task, CancellationToken cancellationToken)
+        {
+            if (task == null)
+                throw new NullReferenceException();
+
+            if (!cancellationToken.CanBeCanceled)
+                return task;
+
+            if (cancellationToken.IsCancellationRequested)
+                return FromCanceled<TResult>(cancellationToken);
+
+            return AsCancelableAsync(task, cancellationToken);
+        }
+
+        #endregion
+
+        public static void WaitAndUnwrap(this Task task)
+        {
+            task.GetAwaiter().GetResult();
+        }
+
+        public static TResult WaitAndUnwrap<TResult>(this Task<TResult> task)
+        {
+            return task.GetAwaiter().GetResult();
+        }
+#else
+        static TResult WaitAndUnwrap<TResult>(this Task<TResult> task)
+        {
+            return task.Result;
         }
 #endif
 
@@ -69,163 +239,7 @@ namespace Karambolo.Common
 
         public static Task EndExecuteTask(this IAsyncResult asyncResult)
         {
-#if !NET40
             return ((Task<Task>)asyncResult).WaitAndUnwrap();
-#else
-            return ((Task<Task>)asyncResult).Result;
-#endif
-        }
-
-#if !NETSTANDARD1_0
-        public static Task WaitForExitAsync(this Process process)
-        {
-            var tcs = new TaskCompletionSource<object>();
-            EventHandler exitedHandler = null;
-            exitedHandler = (s, e) =>
-            {
-                process.Exited -= exitedHandler;
-                tcs.TrySetResult(null);
-            };
-
-            process.EnableRaisingEvents = true;
-            process.Exited += exitedHandler;
-
-            return tcs.Task;
-        }
-#endif
-    }
-
-#if !NET40
-    public static partial class TaskUtils
-    {
-        #region Timeout
-        static async Task WithTimeoutAsync(Task task, TimeSpan timeout)
-        {
-            var timeoutTask = Task.Delay(timeout);
-            var completedTask = await Task.WhenAny(task, timeoutTask).ConfigureAwait(false);
-
-            if (completedTask == timeoutTask)
-                throw new TimeoutException();
-
-            await completedTask.ConfigureAwait(false);
-        }
-
-        public static Task WithTimeout(this Task task, TimeSpan timeout)
-        {
-            if (task == null)
-                throw new NullReferenceException();
-
-            return WithTimeoutAsync(task, timeout);
-        }
-
-        static async Task<TResult> WithTimeoutAsync<TResult>(Task<TResult> task, TimeSpan timeout)
-        {
-            var timeoutTask = Task.Delay(timeout);
-            var completedTask = await Task.WhenAny(task, timeoutTask).ConfigureAwait(false);
-
-            if (completedTask == timeoutTask)
-                throw new TimeoutException();
-
-            return await ((Task<TResult>)completedTask).ConfigureAwait(false);
-        }
-
-        public static Task<TResult> WithTimeout<TResult>(this Task<TResult> task, TimeSpan timeout)
-        {
-            if (task == null)
-                throw new NullReferenceException();
-
-            return WithTimeoutAsync(task, timeout);
-        }
-        #endregion
-
-        #region Cancellation
-        sealed class CancellationTokenTaskSource<TResult> : IDisposable
-        {
-            readonly IDisposable _ctr;
-
-            public CancellationTokenTaskSource(CancellationToken cancellationToken)
-            {
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    Task = Cancelled<TResult>();
-                    return;
-                }
-                var tcs = new TaskCompletionSource<TResult>();
-                _ctr = cancellationToken.Register(() => tcs.TrySetCanceled(), useSynchronizationContext: false);
-                Task = tcs.Task;
-            }
-
-            public Task<TResult> Task { get; private set; }
-
-            public void Dispose()
-            {
-                _ctr?.Dispose();
-            }
-        }
-
-        static Task<TResult> Cancelled<TResult>()
-        {
-            var tcs = new TaskCompletionSource<TResult>();
-            tcs.SetCanceled();
-            return tcs.Task;
-        }
-
-        static async Task AsCancellableAsync(Task task, CancellationToken cancellationToken)
-        {
-            using (var ctts = new CancellationTokenTaskSource<object>(cancellationToken))
-                await await Task.WhenAny(task, ctts.Task).ConfigureAwait(false);
-        }
-
-        public static Task AsCancellable(this Task task, CancellationToken cancellationToken)
-        {
-            if (task == null)
-                throw new NullReferenceException();
-
-            if (!cancellationToken.CanBeCanceled)
-                return task;
-
-            if (cancellationToken.IsCancellationRequested)
-                return Cancelled<object>();
-
-            return AsCancellableAsync(task, cancellationToken);
-        }
-
-        static async Task<TResult> AsCancellableAsync<TResult>(Task<TResult> task, CancellationToken cancellationToken)
-        {
-            using (var ctts = new CancellationTokenTaskSource<TResult>(cancellationToken))
-                return await await Task.WhenAny(task, ctts.Task).ConfigureAwait(false);
-        }
-
-        public static Task<TResult> AsCancellable<TResult>(this Task<TResult> task, CancellationToken cancellationToken)
-        {
-            if (task == null)
-                throw new NullReferenceException();
-
-            if (!cancellationToken.CanBeCanceled)
-                return task;
-
-            if (cancellationToken.IsCancellationRequested)
-                return Cancelled<TResult>();
-
-            return AsCancellableAsync(task, cancellationToken);
-        }
-        #endregion
-
-        public static void WaitAndUnwrap(this Task task)
-        {
-            if (task == null)
-                throw new NullReferenceException();
-
-            task.GetAwaiter().GetResult();
-        }
-
-        public static TResult WaitAndUnwrap<TResult>(this Task<TResult> task)
-        {
-            if (task == null)
-                throw new NullReferenceException();
-
-            return task.GetAwaiter().GetResult();
         }
     }
-#endif
 }
---
+++ test/Karambolo.Common.Test/TaskUtilsTest.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Karambolo.Common.Test
+{
+    public class TaskUtilsTest
+    {
+        class TestException : Exception { }
+
+#if NET461 || NETCOREAPP1_0
+        [Fact]
+        public void CompletedTaskTest()
+        {
+            Assert.Equal(TaskStatus.RanToCompletion, TaskUtils.CompletedTask.Status);
+        }
+
+        [Fact]
+        public async Task FromExceptionTest()
+        {
+            var task = TaskUtils.FromException(new TestException());
+            Assert.Equal(TaskStatus.Faulted, task.Status);
+            await Assert.ThrowsAsync<TestException>(() => task);
+
+            var taskWithResult = TaskUtils.FromException<int>(new TestException());
+            Assert.Equal(TaskStatus.Faulted, taskWithResult.Status);
+            await Assert.ThrowsAsync<TestException>(() => taskWithResult);
+        }
+
+        [Fact]
+        public async Task FromCanceledTest()
+        {
+            using (var cts = new CancellationTokenSource())
+            {
+                await Assert.ThrowsAsync<ArgumentOutOfRangeException>(() => TaskUtils.FromCanceled(cts.Token));
+                cts.Cancel();
+                var task = TaskUtils.FromCanceled(cts.Token);
+                Assert.Equal(TaskStatus.Canceled, task.Status);
+                await Assert.ThrowsAsync<TaskCanceledException>(() => task);
+            }
+
+            using (var cts = new CancellationTokenSource())
+            {
+                await Assert.ThrowsAsync<ArgumentOutOfRangeException>(() => TaskUtils.FromCanceled(cts.Token));
+                cts.Cancel();
+                var taskWithResult = TaskUtils.FromCanceled(cts.Token);
+                Assert.Equal(TaskStatus.Canceled, taskWithResult.Status);
+                await Assert.ThrowsAsync<TaskCanceledException>(() => taskWithResult);
+            }
+        }
+#endif
+
+#if NET461
+        [Fact]
+        public async Task WaitForExitAsyncTest()
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo("cmd", "/c echo test")
+                {
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                }
+            };
+
+            process.Start();
+            await process.WaitForExitAsync();
+            Assert.True(process.HasExited);
+
+            var output = process.StandardOutput.ReadToEnd();
+            Assert.Equal("test", output.Trim());
+        }
+#endif
+
+        [Fact]
+        public async Task WithTimeoutTest()
+        {
+            #region Task
+
+            await Task.Delay(100).WithTimeout(1000);
+            await Assert.ThrowsAsync<TimeoutException>(() => Task.Delay(1000).WithTimeout(100));
+
+            #endregion
+
+            #region Task<TResult>
+
+            Assert.True(await Task.Delay(100).ContinueWith(t => true, TaskContinuationOptions.ExecuteSynchronously).WithTimeout(1000));
+            await Assert.ThrowsAsync<TimeoutException>(() => Task.Delay(1000).ContinueWith(t => true, TaskContinuationOptions.ExecuteSynchronously).WithTimeout(100));
+
+            #endregion
+        }
+
+        [Fact]
+        public async Task AsCancelableTest()
+        {
+            #region Task
+
+            await Task.CompletedTask.AsCancelable(CancellationToken.None);
+
+            var cts = new CancellationTokenSource();
+            await Task.CompletedTask.AsCancelable(cts.Token);
+
+            cts = new CancellationTokenSource();
+            cts.Cancel();
+            await Assert.ThrowsAsync<TaskCanceledException>(() => Task.CompletedTask.AsCancelable(cts.Token));
+            await Assert.ThrowsAsync<TaskCanceledException>(() => TaskUtils.AsCancelableAsync(Task.CompletedTask, cts.Token));
+
+            cts = new CancellationTokenSource();
+            cts.CancelAfter(1000);
+            await Task.Delay(100).AsCancelable(cts.Token);
+
+            cts = new CancellationTokenSource();
+            cts.CancelAfter(100);
+            await Assert.ThrowsAsync<TaskCanceledException>(() => Task.Delay(1000).AsCancelable(cts.Token));
+
+            #endregion
+
+            #region Task<TResult>
+
+            Assert.True(await Task.FromResult(true).AsCancelable(CancellationToken.None));
+
+            cts = new CancellationTokenSource();
+            Assert.True(await Task.FromResult(true).AsCancelable(cts.Token));
+
+            cts = new CancellationTokenSource();
+            cts.Cancel();
+            await Assert.ThrowsAsync<TaskCanceledException>(() => Task.FromResult(true).AsCancelable(cts.Token));
+            await Assert.ThrowsAsync<TaskCanceledException>(() => TaskUtils.AsCancelableAsync(Task.FromResult(true), cts.Token));
+
+            cts = new CancellationTokenSource();
+            cts.CancelAfter(1000);
+            Assert.True(await Task.Delay(100).ContinueWith(t => true, TaskContinuationOptions.ExecuteSynchronously).AsCancelable(cts.Token));
+
+            cts = new CancellationTokenSource();
+            cts.CancelAfter(100);
+            await Assert.ThrowsAsync<TaskCanceledException>(() => Task.Delay(1000).ContinueWith(t => true, TaskContinuationOptions.ExecuteSynchronously).AsCancelable(cts.Token));
+
+            #endregion
+        }
+
+        [Fact]
+        public void WaitAndUnwrapTest()
+        {
+            #region Task
+
+            Task.CompletedTask.WaitAndUnwrap();
+
+            var cts = new CancellationTokenSource();
+            cts.Cancel();
+            Assert.Throws<TaskCanceledException>(() => Task.FromCanceled(cts.Token).WaitAndUnwrap());
+
+            Assert.Throws<TestException>(() => Task.FromException(new TestException()).WaitAndUnwrap());
+
+            #endregion
+
+            #region Task<TResult>
+
+            Assert.True(Task.FromResult(true).WaitAndUnwrap());
+
+            cts = new CancellationTokenSource();
+            cts.Cancel();
+            Assert.Throws<TaskCanceledException>(() => Task.FromCanceled<bool>(cts.Token).WaitAndUnwrap());
+
+            Assert.Throws<TestException>(() => Task.FromException<bool>(new TestException()).WaitAndUnwrap());
+
+            #endregion
+        }
+
+        [Fact]
+        public void ExecuteTaskApmTest()
+        {
+            int flag = 0;
+            var asyncResult = TaskUtils.BeginExecuteTask(Task.Delay(100).ContinueWith(t => flag++, TaskContinuationOptions.ExecuteSynchronously), ar =>
+            {
+                Assert.Equal("state", ar.AsyncState);
+                ar.EndExecuteTask().WaitAndUnwrap();
+                Assert.Equal(1, flag);
+            }, "state");
+
+            SpinWait.SpinUntil(() => asyncResult.IsCompleted);
+        }
+    }
+}
