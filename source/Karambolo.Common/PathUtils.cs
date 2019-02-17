0bc30ee891655c2a36aadc96faa915dc917e20e3
Adam Simon <adamosimoni@gmail.com>
2019-02-17 10:21:55 +0100

    PathUtils.MakeValidFileName improved (handles Windows reserved file names) + tests

--- source/Karambolo.Common/PathUtils.cs
+++ source/Karambolo.Common/PathUtils.cs
@@ -1,23 +1,93 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using Karambolo.Common.Properties;
 
 namespace Karambolo.Common
 {
     public static class PathUtils
     {
-        static readonly char[] illegalFileNameChars = Path.GetInvalidFileNameChars().Concat(Path.GetInvalidPathChars()).ToArray();
+        static readonly HashSet<char> illegalFileNameChars;
+        static readonly HashSet<string> reservedFileNames;
+        static readonly Func<string, bool> isReservedFileName;
 
-        public static string MakeValidFileName(string value)
+        static PathUtils()
         {
-            var chars = value.ToCharArray();
+            illegalFileNameChars = new HashSet<char>(Path.GetInvalidFileNameChars().Concat(Path.GetInvalidPathChars()));
 
-            var n = chars.Length;
+            bool isWindowsOS =
+#if NETSTANDARD2_0
+                RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+#else
+                true;
+#endif
+
+            if (isWindowsOS)
+            {
+                reservedFileNames = new HashSet<string>
+                {
+                    "con", "prn", "aux", "nul",
+                    "com1", "com2", "com3", "com4", "com5", "com6", "com7", "com8", "com9",
+                    "lpt1", "lpt2", "lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8", "lpt9",
+                };
+                isReservedFileName = IsReservedFileNameWindows;
+            }
+            else
+                isReservedFileName = IsReservedFileNameOther;
+
+        }
+
+        static bool IsReservedFileNameWindows(string value)
+        {
+            return reservedFileNames.Contains(Path.GetFileNameWithoutExtension(value));
+        }
+
+        static bool IsReservedFileNameOther(string value)
+        {
+            return false;
+        }
+
+        public static bool IsReservedFileName(string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            return isReservedFileName(value);
+        }
+
+        /// <remarks>
+        /// Maximum length of filenames depends on the underlying filesystem.
+        /// <seealso href="https://serverfault.com/questions/9546/filename-length-limits-on-linux" />
+        /// </remarks>
+        public static string MakeValidFileName(string value, int maxLength = 255)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            if (value.Length == 0)
+                throw new ArgumentException(Resources.InvalidValue, nameof(value));
+
+            if (maxLength <= 0)
+                throw new ArgumentOutOfRangeException(nameof(maxLength));
+
+            if (value.FindIndex(c => !char.IsWhiteSpace(c)) < 0)
+                return new string('_', value.Length);
+
+            char[] chars = null;
+            var n = Math.Min(value.Length, maxLength);
             for (var i = 0; i < n; i++)
-                if (Array.IndexOf(illegalFileNameChars, chars[i]) >= 0)
-                    chars[i] = '_';
+                if (illegalFileNameChars.Contains(value[i]))
+                    (chars ?? (chars = value.ToCharArray()))[i] = '_';
 
-            return new string(chars);
+            if (chars != null)
+                return new string(chars);
+
+            if (isReservedFileName(value))
+                return "_" + value;
+
+            return value.Truncate(maxLength);
         }
     }
 }
---
+++ test/Karambolo.Common.Test/PathUtilsTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Karambolo.Common
+{
+    public class PathUtilsTest
+    {
+#if NET461
+        [Fact]
+        public void IsReservedFileNameTest()
+        {
+            Assert.True(PathUtils.IsReservedFileName("aux"));
+            Assert.True(PathUtils.IsReservedFileName("aux."));
+            Assert.True(PathUtils.IsReservedFileName("aux.xyz"));
+            Assert.False(PathUtils.IsReservedFileName("_con"));
+        }
+#endif
+
+        [Fact]
+        public void MakeValidFileNameTest()
+        {
+            Assert.Equal("__", PathUtils.MakeValidFileName("  "));
+            Assert.Equal(" a ", PathUtils.MakeValidFileName(" a "));
+
+#if NET461
+            Assert.Equal("_nul", PathUtils.MakeValidFileName("nul"));
+            Assert.Equal("_prn.txt", PathUtils.MakeValidFileName("prn.txt"));
+#endif
+
+            Assert.Equal("_some_invalid_filename_", PathUtils.MakeValidFileName("*some/invalid|filename?"));
+            Assert.Same("This should be a valid file name.", PathUtils.MakeValidFileName("This should be a valid file name."));
+
+            Assert.Equal(
+                "0123456789abcdef_123456789abcde",
+                PathUtils.MakeValidFileName("0123456789abcdef?123456789abcde", 31));
+
+            Assert.Equal(
+                "0123456789abcdef0123456789abcde",
+                PathUtils.MakeValidFileName("0123456789abcdef0123456789abcdef", 31));
+        }
+    }
+}
